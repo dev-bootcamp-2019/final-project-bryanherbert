930e453c426890f4e20fd97a008df661fb73f332
bryanherbert <bryan.herbert1124@gmail.com>
2018-12-11 10:54:13 -0500

    Removed simplestorage files

--- truffle.js
+++ truffle.js
@@ -3,8 +3,8 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  //will need to re-incorporate below line for something
-  //contracts_build_directory: path.join(__dirname, "client/src/contracts")
+  
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
 
   networks:{
     development:{
