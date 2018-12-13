a9162ea92dfe68ab2c56b06dbd8de4ef105c3031
bryanherbert <bryan.herbert1124@gmail.com>
2018-12-13 01:12:07 -0500

    Revised strategy struct and updated functions and tests to inclue quant-inputed fees

--- contracts/StrategyHub.sol
+++ contracts/StrategyHub.sol
@@ -14,10 +14,15 @@ contract StrategyHub {
         address stratOwner;
         //amount of funds the strategy is virtually managing
         uint funds;
+        //Add fee that quant can set- in whole number, i.e. 2% is represented as 2
+        uint feeRate;
         //maps investors to investment status- current investors return true, non-investors return false
         mapping (address => bool) investors;
         //maps investors to their virtual balances in the strategy
-        mapping (address => uint) balances;
+        mapping (address => uint) virtualBalances;
+        //maps investors to the actual fee they have stored in the Strategy Hub contract
+        //fees are paid into stratOwner fee account and paid from investor fee account
+        mapping (address => uint) fees;
         //will need to add IPFS hash eventually to verify code
 
     }
@@ -29,10 +34,10 @@ contract StrategyHub {
     );
 
     //Modifiers
-    modifier verifyBalance(address _account, uint _investment){
+    modifier verifyBalance(uint _stratNum, address _account, uint _investment){
         //Account Balance must be greater than investment + Fees
         //Not sure this correct- want it to represent ~2%
-        uint fee = (_investment / 50) + 1;
+        uint fee = _investment/checkFeeRate(_stratNum);
         require(
             _account.balance > _investment + fee,
             "Sender does not have enough balance to invest"
@@ -40,10 +45,10 @@ contract StrategyHub {
         _;
     }
 
-    modifier verifyFee(uint _investment, uint _proposedFee) {
+    modifier verifyFee(uint _stratNum, uint _investment, uint _proposedFee) {
         //Verify that the msg.value > fee
         require(
-            _proposedFee >= _investment/50+1,
+            _proposedFee >= _investment/checkFeeRate(_stratNum),
             "Fee is insufficent"
         );
         _;
@@ -55,17 +60,21 @@ contract StrategyHub {
         stratCount = 0;
     }
 
-    function initializeStrat(bytes32 _name) public payable {
+    function initializeStrat(bytes32 _name, uint _investment, uint _feeRate) public payable {
         //initialize strat name to _name
         strategies[stratCount].name = _name;
         //Strat owner is message sender
         strategies[stratCount].stratOwner =  msg.sender;
         //Initial funds are the msg.value
-        strategies[stratCount].funds = msg.value;
+        strategies[stratCount].funds = _investment;
+        //Set fee rate
+        strategies[stratCount].feeRate = _feeRate;
         //set stratOwner to also be an investor
         strategies[stratCount].investors[msg.sender] = true;
-        //set stratOwner's investor balance to the msg. value
-        strategies[stratCount].balances[msg.sender] = msg.value;
+        //set stratOwner's investor balance to the msg.value
+        strategies[stratCount].virtualBalances[msg.sender] = _investment;
+        //set stratOwner's fees to zero
+        strategies[stratCount].fees[msg.sender] = 0;
         //Emit Event
         emit StrategyCreated(stratCount, msg.sender);
         //Increment stratCount
@@ -81,21 +90,32 @@ contract StrategyHub {
     //Make investment into particular fund
     //Must have required funds
     function Invest(uint _stratNum, uint _investment) public payable 
-    verifyBalance(msg.sender, _investment) 
-    verifyFee(_investment, msg.value) 
+    verifyBalance(_stratNum, msg.sender, _investment) 
+    verifyFee(_stratNum, _investment, msg.value) 
     returns (bool) {
+        strategies[_stratNum].funds += _investment;
         strategies[_stratNum].investors[msg.sender] = true;
-        strategies[_stratNum].balances[msg.sender] = _investment;
+        strategies[_stratNum].virtualBalances[msg.sender] = _investment;
+        strategies[_stratNum].fees[msg.sender] = msg.value;
         return strategies[_stratNum].investors[msg.sender];
     }
 
-    //not a permanent function
-    function getStratDetails(uint _stratNum, address _addr) public view returns (bytes32, address, uint, bool, uint){
-        return (strategies[_stratNum].name, 
-        strategies[0].stratOwner, 
-        strategies[0].funds, 
-        strategies[0].investors[_addr], 
-        strategies[0].balances[_addr]);
+    //check fee rate
+    function checkFeeRate(uint _stratNum) public view returns (uint) {
+        return 100/strategies[_stratNum].feeRate;
     }
-  
+
+    //not a permanent function
+    function getStratDetails(uint _stratNum) public view returns (bytes32, address, uint, uint){
+        return (strategies[_stratNum].name, 
+        strategies[_stratNum].stratOwner, 
+        strategies[_stratNum].funds, 
+        strategies[_stratNum].feeRate);
+    }
+
+    function getStratDetails2(uint _stratNum, address _addr) public view returns (bool, uint, uint){
+        return(strategies[_stratNum].investors[_addr], 
+        strategies[_stratNum].virtualBalances[_addr],
+        strategies[_stratNum].fees[_addr]);
+    }
 }
--- test/TestStrategyHub.sol
+++ test/TestStrategyHub.sol
@@ -14,8 +14,10 @@ contract TestStrategyHub {
     bytes32 a;
     address b;
     uint c;
-    bool d;
-    uint e;
+    uint d;
+    bool e;
+    uint f;
+    uint g;
 
   function beforeAll(){
     //Deploy StrategyHub contracts
@@ -34,47 +36,59 @@ contract TestStrategyHub {
     address quantAddress = address(quant);
     uint initialFund = 1 ether;
     uint count = 0;
-    quant.initializeStrategy(s, name, initialFund);
+    uint feeRate = 2;
+    quant.initializeStrategy(s, name, initialFund, feeRate);
 
-    (a,b,c,d,e) = s.getStratDetails(count, quantAddress);
+    (a,b,c,d) = s.getStratDetails(count);
+    (e,f,g) = s. getStratDetails2(count, quantAddress);
 
     //Tests
     Assert.equal(a, name, "Strategy name does not match test name");
     Assert.equal(b, quantAddress, "Quant is not owner of strategy");
     Assert.equal(c, initialFund, "Strategy funds do not match test funds");
-    Assert.equal(d, true, "Quant is not listed as investor");
-    Assert.equal(e, initialFund, "Quant's funds are not listed");
+    Assert.equal(d, feeRate, "Fee Rate does not match test rate");
+    Assert.equal(e, true, "Quant is not listed as investor");
+    Assert.equal(f, initialFund, "Quant's funds are not listed");
+    Assert.equal(g, 0, "Quant's fees deposited are not zero");
   }
 
   function testIsInvestor(){
     //Check to see if account is an investor in a certain strategy
     address investorAddr = address(investor);
     uint stratNum = 0;
-    investor.checkInvestmentStatus(s, stratNum);
+    bool isInvestor = investor.checkInvestmentStatus(s, stratNum);
     uint investment = 2 ether;
 
-    (,,,d,e) = s.getStratDetails(stratNum, investorAddr);
+    (,,c,) = s.getStratDetails(stratNum);
+    (e,f,g) = s.getStratDetails2(stratNum, investorAddr);
 
     //Tests
-    Assert.equal(d, false, "Account is incorrectly listed as investor");
-    Assert.equal(e, 0, "Account balance should be empty");
+    Assert.equal(isInvestor, false, "Account is incorrectly listed as investor");
+    Assert.equal(c, 1 ether, "Initial account fund does not match initial balance");
+    Assert.equal(e, false, "Account is incorrectly listed as investor");
+    Assert.equal(f, 0, "Investor's virtual balance is not zero");
+    Assert.equal(g, 0, "Investor's fees are not zero");
 
     //Make an actual investment
     investor.makeInvestment(s, stratNum, investment);
 
-    (,,,d,e) = s.getStratDetails(stratNum, investorAddr);
+    //Tests
+    (,,c,) = s.getStratDetails(stratNum);
+    (e,f,g) = s.getStratDetails2(stratNum, investorAddr);
 
     //Tests
-    Assert.equal(d, true, "Account is incorrectly listed as not an investor");
-    Assert.equal(e, investment, "Account balance is not empty");
+    Assert.equal(c, 3 ether, "Funds do not match sum of virtual balances");
+    Assert.equal(e, true, "Account is not listed as investor");
+    Assert.equal(f, 2 ether, "Investor's virtual balance does not match investment");
+    Assert.equal(g, (investment/s.checkFeeRate(stratNum)+1), "Investor's fees were not valid");
   }
 
 }
 
 contract Quant {
 
-    function initializeStrategy(StrategyHub strategyHub, bytes32 _name, uint _fund) public {
-        strategyHub.initializeStrat.value(_fund)(_name);
+    function initializeStrategy(StrategyHub strategyHub, bytes32 _name, uint _initalFund, uint _feeRate) public {
+        strategyHub.initializeStrat(_name, _initalFund, _feeRate);
     }
 
     //Fallback function, accepts ether
@@ -86,13 +100,12 @@ contract Quant {
 
 contract Investor {
 
-    function checkInvestmentStatus(StrategyHub s, uint _stratNum) public {
-        s.isInvestor(_stratNum);
+    function checkInvestmentStatus(StrategyHub s, uint _stratNum) public view returns (bool) {
+        return s.isInvestor(_stratNum);
     }
 
     function makeInvestment(StrategyHub s, uint _stratNum, uint _investment) public {
-        //2% fee
-        uint fee = _investment/50 + 1;
+        uint fee = _investment/s.checkFeeRate(_stratNum) + 1;
         s.Invest.value(fee)(_stratNum, _investment);
     }
 
