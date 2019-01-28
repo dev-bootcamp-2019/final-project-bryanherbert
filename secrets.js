886faaac7fd1dd030b9fddfad44775222db0ebab
bryanherbert <bryan.herbert1124@gmail.com>
2019-01-28 16:10:02 -0500

    readded secret.js

---
+++ secrets.js
@@ -0,0 +1,5 @@
+var infuraKey = "560f746d08c04a2e91747019632e5680";
+var MNEMONIC = "siren final blouse cherry home climb paper morning mistake crater actress caught";
+//var ropstenPK = accountPK;
+
+module.exports = {infuraKey: infuraKey, MNEMONIC: MNEMONIC};
